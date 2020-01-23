150649184ba7957d74f76c6b45995d0b520ee5ab
Caleb Bartel <cbartel@mymail.mines.edu>
2020-01-22 17:43:01 -0700

    Added second syso

--- src/GitFun.java
+++ src/GitFun.java
@@ -4,6 +4,7 @@ public class GitFun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Halpeth");
 	}
 
 }
